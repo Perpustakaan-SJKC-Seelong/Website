f4cfa38ab6f76faa7781b8b901f45d2a74422973
Shawn.S <93225931+zikangS@users.noreply.github.com>
2025-02-12 14:18:00 +0100

    School News

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
 // News Page Functionality
 function initializeNewsPage() {
     // Sample news data (replace with actual data from API/database)
@@ -35,8 +36,17 @@ function initializeNewsPage() {
             id: 1,
             title: "Rakan Inklusif 2024/2025",
             excerpt: "每一个特教孩子的进步和成长，离不开你们的耐心和关爱。感谢家长们愿意提供机会...",
-            date: "2025-01-17",
-            image: "images/school_news/rakan_inklusif_tahun_dua.jpg"
+            date: "17 Jan 2025",
+            image: "images/school_news/rakan_inklusif_tahun_dua.jpg",
+            category: "Award"
+        },
+        {
+            id: 2,
+            title: "Makluman Waktu Persekolahan 2025",
+            excerpt: "Makluman Waktu Persekolahan, Waktu Rehat dan Waktu Balik bagi ...",
+            date: "03 Feb 2025",
+            image: "images/school_news/makluman_3Feb.jpg",
+            category: "Notis"
         }
     ];
 
@@ -45,20 +55,38 @@ function initializeNewsPage() {
     // Generate news items
     newsData.forEach(news => {
         const newsItem = document.createElement('div');
-        newsItem.className = 'news-item card mb-3';
+        newsItem.className = 'news-item mb-3';
         newsItem.innerHTML = `
-            <div class="card-body">
-                <h3 class="card-title">${news.title}</h3>
-                <p class="card-text">${news.excerpt}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <a href="news-detail.html?id=${news.id}" class="btn btn-primary">Read More</a>
-                    <span class="news-date">${news.date}</span>
+            <div class="col-lg-12 col-md-6">
+              <div class="item">
+                <div class="row">
+                  <div class="col-lg-3">
+                    <div class="image">
+                      <img src=${news.image} alt="">
+                    </div>
+                  </div>
+                  <div class="col-lg-9">
+                    <ul>
+                      <li>
+                        <span class="category">${news.category}</span>
+                        <h4>${news.title}</h4>
+                      </li>
+                      <li>
+                        <span>Date:</span>
+                        <h6>${news.date}</h6>
+                      </li>
+                    </ul>
+                    <a href="news-detail.html?id=${news.id}"><i class="fa fa-angle-right"></i></a>
+                  </div>
                 </div>
+              </div>
             </div>
+
         `;
         newsContainer.appendChild(newsItem);
     });
 }
+
 
 // News Detail Page Functionality
 function initializeNewsDetailPage() {
@@ -72,32 +100,41 @@ function initializeNewsDetailPage() {
             title: "Rakan Inklusif 2024/2025",
             date: "2025-01-17",
             content: `
-                <p>每一个特教孩子的进步和成长，离不开你们的耐心和关爱。感谢家长们愿意提供机会给予这些孩子们来帮助特教孩子。融合教育为特教孩子们提供了一个平等、包容和鼓励的学习环境，使他们能够充分发挥自己的潜力，克服挑战，收获自信与成就。</p>
+                <h6>每一个特教孩子的进步和成长，离不开你们的耐心和关爱。感谢家长们愿意提供机会给予这些孩子们来帮助特教孩子。融合教育为特教孩子们提供了一个平等、包容和鼓励的学习环境，使他们能够充分发挥自己的潜力，克服挑战，收获自信与成就。</h6>
                 <img src="images/school_news/rakan_inklusif_tahun_dua.jpg" class="news-image" alt="Rakan Inklusif Tahun Dua">
                 <img src="images/school_news/rakan_inklusif_tahun_lima.jpg" class="news-image" alt="Rakan Inklusif Tahun Lima">
-                <p>以下是rakan Inklusif 的得奖者：</p>
-                <p>Rakan Inklusif Tahun Dua:</p>
+                <h6>以下是rakan Inklusif 的得奖者：</h6>
+                <br/>
+                <h6>Rakan Inklusif Tahun Dua:</h6>
                 <ul>
-                    <li>Queck Chen Ler</li>
-                    <li>Tey Yu Tong</li>
-                    <li>Yong Jing Yee</li>
-                    <li>Chiow Jia Hui</li>
-                    <li>Taashvin Naidu</li>
-                    <li>Queenie Ong Xin Rui</li>
-                    <li>Lim Kai Rui</li>
-                    <li>Chong Yon Wei</li>
-                    <li>Chai Zhi Rou</li>
+                    <li>- Queck Chen Ler</li>
+                    <li>- Tey Yu Tong</li>
+                    <li>- Yong Jing Yee</li>
+                    <li>- Chiow Jia Hui</li>
+                    <li>- Taashvin Naidu</li>
+                    <li>- Queenie Ong Xin Rui</li>
+                    <li>- Lim Kai Rui</li>
+                    <li>- Chong Yon Wei</li>
+                    <li>- Chai Zhi Rou</li>
                 </ul>
-
-                <p>Rakan Inklusif Tahun Lima:</p>
+                <br/>
+                <h6>Rakan Inklusif Tahun Lima:</h6>
                 <ul>
-                    <li>Queck Ler Tong</li>
-                    <li>Tey Rui Hang</li>
-                    <li>Kenzo Low</li>
-                    <li>Wong Eugene</li>
-                    <li>Wong Euroye</li>
-                    <li>Carlise Yong</li>
+                    <li>- Queck Ler Tong</li>
+                    <li>- Tey Rui Hang</li>
+                    <li>- Kenzo Low</li>
+                    <li>- Wong Eugene</li>
+                    <li>- Wong Euroye</li>
+                    <li>- Carlise Yong</li>
                 </ul>
+            `,
+        },
+        2: {
+            title: "Makluman Waktu Persekolahan 2025",
+            date: "2025-02-03",
+            content: `
+                <h6>Makluman Waktu Persekolahan, Waktu Rehat dan Waktu Balik bagi sesi persekolahan 2025</h6>
+                <img src="images/school_news/makluman_3Feb.jpg" class="news-image" alt="Makluman Waktu Persekolahan 2025">
             `,
         }
     };
